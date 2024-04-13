10e5151745b0a03ce99aced136248ca1a5a064b8
napasorn <napasorn.tsp@gmail.com>
2024-04-13 17:19:28 +0700

    submit: JavaScript Loop Assignment

--- ex-6.js
+++ ex-6.js
@@ -2,6 +2,9 @@
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
-// Start coding here
+for (let i = 0; i < companyName.length; i++) {
+    reversedCompanyName = companyName[i] + reversedCompanyName;
+  }
+
 
 console.log(reversedCompanyName);
